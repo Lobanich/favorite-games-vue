ac197e5f5eda34627b85796e3e65b1feb836473e
Konstantin <tomashov,kostyan@icloud.com>
2024-07-23 17:51:11 +0300

    improvements

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-const { createApp } = Vue;
+const { createApp, ref, computed } = Vue;
 
 createApp({
-    // @todo используй setup-функцию вместо data https://vuejs.org/guide/introduction.html
-    data() {
-        return {
-            // @todo для вычисления жанров используй computed https://vuejs.org/guide/essentials/computed.html
-            genres: ["Action", "RPG", "Strategy"],
-            // @todo перепиши все данные на такую же структуру, как была в прошлой репе
-            games: {
-                Action: [
+    setup() {
+        const games = ref([
+            {
+                genre: "Action",
+                games: [
                     { title: "DOOM", description: "Классический шутер от первого лица" },
                     { title: "Half-Life 2", description: "Революционный шутер с захватывающим сюжетом" },
                     { title: "Uncharted 4", description: "Приключенческий экшн с потрясающей графикой и сюжетом" },
                     { title: "Call of Duty", description: "Популярная военная игра" },
                     { title: "GTA V", description: "Игра с открытым миром и захватывающим сюжетом" },
                     { title: "Red Dead Redemption 2", description: "Приключенческий экшн в открытом мире" }
-                ],
-                RPG: [
+                ]
+            },
+            {
+                genre: "RPG",
+                games: [
                     { title: "The Witcher 3", description: "Эпическая RPG с открытым миром" },
                     { title: "Mass Effect 2", description: "Научно-фантастическая RPG с захватывающим сюжетом" },
                     { title: "Skyrim", description: "Легендарная RPG с открытым миром" },
                     { title: "Dragon Age: Inquisition", description: "Фэнтезийная RPG с увлекательной историей" },
                     { title: "Cyberpunk 2077", description: "Futuristic RPG with open world" },
                     { title: "Divinity: Original Sin 2", description: "Классическая RPG с пошаговыми боями" }
-                ],
-                Strategy: [
+                ]
+            },
+            {
+                genre: "Strategy",
+                games: [
                     { title: "StarCraft", description: "Культовая стратегия в реальном времени" },
                     { title: "Civilization VI", description: "Глобальная пошаговая стратегия" },
                     { title: "Age of Empires II", description: "Классическая стратегия в реальном времени" },
@@ -33,6 +36,15 @@ createApp({
                     { title: "Warcraft III", description: "Классическая стратегия в реальном времени" }
                 ]
             }
+        ]);
+
+        const navGenres = computed(() => {
+            return games.value.map(genre => genre.genre);
+        });
+
+        return {
+            games,
+            navGenres
         };
     }
 }).mount('#app');
